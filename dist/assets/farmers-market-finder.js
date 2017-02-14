0da46bfac68dbea078c8629b50410906d92c017c
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-02-13 23:57:59 -0800

    Adds map style from SnazzyMaps

--- dist/assets/farmers-market-finder.js
+++ dist/assets/farmers-market-finder.js
@@ -28,7 +28,25 @@ define('farmers-market-finder/components/google-map', ['exports', 'ember'], func
 			var container = this.$('#map')[0];
 			var options = {
 				center: new window.google.maps.LatLng(this.get('latitude'), this.get('longitude')),
-				zoom: 4
+				zoom: 4,
+				styles: [{
+					"featureType": "landscape", "stylers": [{ "hue": "#FFA800" }, { "saturation": 0 }, { "lightness": 0 }, { "gamma": 1 }]
+				}, {
+					"featureType": "road.highway",
+					"stylers": [{ "hue": "#53FF00" }, { "saturation": -73 }, { "lightness": 40 }, { "gamma": 1 }]
+				}, {
+					"featureType": "road.arterial",
+					"stylers": [{ "hue": "#FBFF00" }, { "saturation": 0 }, { "lightness": 0 }, { "gamma": 1 }]
+				}, {
+					"featureType": "road.local",
+					"stylers": [{ "hue": "#00FFFD" }, { "saturation": 0 }, { "lightness": 30 }, { "gamma": 1 }]
+				}, {
+					"featureType": "water",
+					"stylers": [{ "hue": "#00BFFF" }, { "saturation": 6 }, { "lightness": 8 }, { "gamma": 1 }]
+				}, {
+					"featureType": "poi",
+					"stylers": [{ "hue": "#679714" }, { "saturation": 33.4 }, { "lightness": -25.4 }, { "gamma": 1 }]
+				}]
 			};
 			new window.google.maps.Map(container, options);
 		}).on('didInsertElement')
